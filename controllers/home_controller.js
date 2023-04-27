37dd67c8457b92028cedb92ca09a34c8e4a8a35a
Simran Mandal <simranmandal0211@gmail.com>
2023-04-27 16:01:11 +0530

    create home.ejs as a view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 module.exports.home = function(request, respond){
-    return respond.end('<h1>Express is up for Codeal!</h1>');
+    
+    return respond.render('home', {
+        title: "home"
+    });
+    // return respond.end('<h1>Express is up for Codeal!</h1>');
 }
 
 //module.exports.actionName = function(request, respond){}
