7e2d5f3d34cfa4197c185c948a3f7311afb3e45f
Simran Mandal <simranmandal0211@gmail.com>
2023-05-07 16:54:37 +0530

    Add Action to User SignUp

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/user');
+
+
 module.exports.profile = function(request, respond){
     // return respond.end('<h1>User Profile</h1>');
 
@@ -22,10 +25,27 @@ module.exports.signIn = function(request, respond){
 
 // get the signUp data
 module.exports.create = function(request, respond){
-    // TODO Later
+    // first we check pswd & confirm pswd same or not
+    if(request.body.password != request.body.confirm_password){
+        return respond.redirect('back');
+    }
+    User.findOne({email: request.body.email}, function(err, user){
+        if(err){    console.log('error in finding in signing up');  return; }
+
+        if(!user){
+            User.create(request.body, function(err, user){
+                if(err){    console.log('error in creating user while signing up');
+                    return}
+
+                return respond.redirect('/users/sign-in');
+            })
+        }else{  //if user already exist we send back to sign-in page
+            return respond.redirect('back');
+        }
+    });
 }
 
 // get the signIn data
 module.exports.createSession = function(request, respond){
-    // TODO Later
+    //TODO Later
 }
