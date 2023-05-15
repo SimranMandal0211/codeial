e56f317b2f2352bfbf275f5f0bf42959b9f4ca1c
Simran Mandal <simranmandal0211@gmail.com>
2023-05-15 23:52:34 +0530

    Create Schema for Comments

---
+++ models/comment.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const connectSchema = new mongoose.Schema({
+    content: {
+        type: String, 
+        require: true
+    },
+// comment belong to a user
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }
+},{
+    timestamps: true
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
