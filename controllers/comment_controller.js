4910981c083c443b3be9ae4a1be3de2bbdf421b6
Simran Mandal <simranmandal0211@gmail.com>
2023-06-09 15:12:03 +0530

    Install multer

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -2,37 +2,58 @@ const Comment = require('../models/comment');
 const Post = require('../models/post');
 
 module.exports.create = async function(request, respond){
-    let post = await Post.findById(request.body.post);
+
+    try{
+        let post = await Post.findById(request.body.post);
          
-    if(post){
-        Comment.create({
-            content: request.body.content,
-            post: request.body.post,
-            user:request.user._id
-        },function(err, comment){
-         // handle error
+        if(post){
+            let comment = Comment.create({
+                content: request.body.content,
+                post: request.body.post,
+                user:request.user._id
+            });
+
             post.comments.push(comment);
             post.save();
  
             request.flash('success', 'Added Comment!');
             respond.redirect('/');
-        });
+        }
+    }catch(err){
+        request.flash('error', err);
+        return;
     }
+    
 }
 
-module.exports.destroy = function(request, respond){
-    Comment.findById(request.params.id, function(err, comment){
+module.exports.destroy = async function(request, respond){
+    try{
+        let comment = await Comment.findById(request.params.id);
         if(comment.user == request.user.id){
             let postId = comment.post;
             comment.remove();
 
-            Post.findByIdAndUpdate(postId, { $pull : {comments: request.params.id}}, function(err, post){
-                request.flash('success', 'Comment deleted');
-                return respond.redirect('back');
-            })
+            let post = await Post.findByIdAndUpdate(postId, { $pull : {comments: request.params.id}});
+            
+            // send the comment id which was deleted back to the views
+            if(request.xhr){
+                return respond.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Comment deleted"
+                });
+            }
+
+            request.flash('success', 'Comment deleted');
+            return respond.redirect('back');
+
         }else{
             request.flash('error', 'You can not delete this comment!');
             return respond.redirect('back');
         }
-    });
+    }catch(err){
+        req.flash('error', err);
+        return;
+    }
 }
