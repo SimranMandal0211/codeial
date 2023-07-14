8766ff1f73cabc3e6e67749e70c7527ea5019bf5
Simran Mandal <simranmandal0211@gmail.com>
2023-07-14 18:31:23 +0530

    ChatBox: Send and Receive Messages

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -7,9 +7,9 @@ class ChatEngine{
 
 // this tell go and connect
 // 1..
-        this.socket = io.connect('http://localhost:5000'); //io - is gloable variable by socket.io file
+    this.socket = io.connect('http://localhost:5000'); //io - is gloable variable by socket.io file
 
-        if(this.userEmail){
+        if (this.userEmail){
             this.connectionHandler();
         }
     }
@@ -28,8 +28,44 @@ class ChatEngine{
 
             self.socket.on('user_joined', function(data){
                 console.log('a user joined!', data);
-            })
+            });
 
         });
+
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+
+            if (msg != ''){
+                self.socket.emit('send_message', {
+                    message: msg,
+                    user_email: self.userEmail,
+                    chatroom: 'codeial'
+                });
+            }
+        });
+        
+        self.socket.on('receive_message', function(data){
+            console.log('message received', data.message);
+
+            let newMessage = $('<li>');
+
+            let messageType = 'other-message';
+
+            if (data.user_email == self.userEmail){
+                messageType = 'self-message';
+            }
+
+            newMessage.append($('<span>', {
+                'html': data.message
+            }));
+
+            newMessage.append($('<sub>', {
+                'html': data.user_email
+            }));
+
+            newMessage.addClass(messageType);
+
+            $('#chat-messages-list').append(newMessage);
+        })
     }
 }
