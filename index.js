1d76df0770ae3bc01adb1effff60d67378aff21b
Simran Mandal <simranmandal0211@gmail.com>
2023-07-14 13:40:24 +0530

    Setting up socket.io

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
+// setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('Chat server is lisening on port 5000');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
