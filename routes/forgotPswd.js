2beb337b1f4b6a71df999fccfa6afc8aa3a5a723
Simran Mandal <simranmandal0211@gmail.com>
2023-07-23 17:50:35 +0530

    router, controller and views: Add reset-password feature

--- routes/forgotPswd.js
+++ routes/forgotPswd.js
@@ -5,6 +5,11 @@ const router = express.Router();
 const forgotPaswdController = require('../controllers/forgotPaswd_controller');
 
 router.get('/forgot-password', forgotPaswdController.forgot);
+router.post('/forgot-password', forgotPaswdController.setEmail);
+
+router.get('/reset-password/:token',forgotPaswdController.getResetPaswd);
+router.post('/reset-password/:token',forgotPaswdController.resetPaswd);
+
 
 module.exports = router;
 
