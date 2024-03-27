8b40de96713f396d5a4bc99c2101282c82968ab2
Simran Mandal <simranmandal0211@gmail.com>
2024-03-27 16:06:37 +0530

    add redis to kue

--- config/kue.js
+++ config/kue.js
@@ -1,4 +1,12 @@
 const kue = require('kue');
-const queue = kue.createQueue();
+// const queue = kue.createQueue();
+
+var queue = kue.createQueue({
+    redis: {
+        port: 6379, // Redis port
+        host: '127.0.0.1', // Redis host
+        // Other Redis options if needed
+    }
+});
 
 module.exports = queue;
